87f7b4f976825c9f0142d7363335adfc151dd213
ibrhmkurt <ibrahimkurt@proton.me>
2023-02-23 18:15:45 +0300

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Campaign</title>
+</head>
+<body>
+<h1>Discount Code Csv import</h1>
+
+@if(session('success'))
+    <p>{{ session('success') }}</p>
+@endif
+
+<form method="post" enctype="multipart/form-data">
+    @csrf
+    <label for="csv_file">Code Csv File</label>
+    <br>
+    <input type="file" name="csv_file" id="csv_file">
+    <br><br>
+    <button type="submit">Upload</button>
+</form>
+
+<br>
+<br>
+<hr>
+
+<table>
+    <thead>
+        <tr>
+            <th>Discount Code</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($discount_codes as $code)
+            <tr>
+                <th>{{$code->discount_code}}</th>
+            </tr>
+        @endforeach
+    </tbody>
+
+</table>
+
+<br>
+<hr>
+
+{{ $discount_codes->links() }}
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index', [
+        'discount_codes' => \App\Models\Code::paginate(),
+    ]);
+});
+
+Route::post('/', function () {
+
+    $csv = \League\Csv\Reader::createFromPath(request()->file('csv_file')->getRealPath());
+    $csv->setHeaderOffset(0);
+
+
+    $discount_codes = [];
+
+    foreach ($csv as $record) {
+        $discount_codes[] = [
+            'discount_code' => $record['Code'],
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+
+        if (count($discount_codes) === 5000) {
+            \App\Models\Code::insert($discount_codes);
+
+            $discount_codes = [];
+        }
+    }
+
+    return redirect()->back()->with('success', 'All Discount Code imported!!');
+});
+
+
